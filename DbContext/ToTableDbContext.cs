8e13a70e9af09afa276318de4db7cf0a4d06297c
Mateusz Placha <mateusz.placha@test.com>
2024-01-19 17:06:11 +0100

    Fix appsettings.json and dbcontext

--- DbContext/ToTableDbContext.cs
+++ DbContext/ToTableDbContext.cs
@@ -5,9 +5,7 @@ namespace ToTable.Models
     public class ToTableDbContext : DbContext
     {
         public ToTableDbContext(DbContextOptions<ToTableDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
         public DbSet<Order> OrderItems { get; set; } = null!;
         public DbSet<OrderItem> OrderItemItems { get; set; } = null!;
@@ -16,37 +14,5 @@ namespace ToTable.Models
         public DbSet<Table> TableItems { get; set; } = null!;
         public DbSet<Waiter> WaiterItems { get; set; } = null!;
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Konfiguracja relacji między Order a Payment
-            modelBuilder.Entity<Order>()
-                .HasOne(o => o.Payment)
-                .WithOne(p => p.Order)
-                .HasForeignKey<Payment>(p => p.OrderId)
-                .OnDelete(DeleteBehavior.Cascade); // Opcjonalne - określa zachowanie kasowania w przypadku usunięcia zamówienia
-
-            // Konfiguracja relacji między OrderItem a Order
-            modelBuilder.Entity<OrderItem>()
-                .HasOne(oi => oi.Order)
-                .WithMany(o => o.OrderItems)
-                .HasForeignKey(oi => oi.OrderId)
-                .OnDelete(DeleteBehavior.Cascade); // Opcjonalne - określa zachowanie kasowania w przypadku usunięcia zamówienia
-        }
     }
 }
-
-
-    
-    
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     base.OnModelCreating(modelBuilder);
-    //
-    //     modelBuilder.Entity<Order>()
-    //         .WithOne(p => p.Order)
-    //                     .HasForeignKey<Payment>(p => p.Order); .HasOne(o => o.Payment)
-    //         
-    // }
-
