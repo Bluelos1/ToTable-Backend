29ae35d401a657ccf2b18405cc9309de7334d0b9
Bluelos1 <s25946@pjwstk.edu.pl>
2024-01-21 22:23:40 +0100

    Allow null to waiter

--- Contract/OrderDto.cs
+++ Contract/OrderDto.cs
@@ -9,7 +9,7 @@ public class OrderDto
     public OrderStatus OrderStatus { get; set; }
     public string? OrderComment { get; set; }
     public string PaymentMethod { get; set; }
-    public int WaiterId { get; set; }
+    public int? WaiterId { get; set; }
     public int TableId { get; set; }
     public int RestaurantId { get; set; }
 }
