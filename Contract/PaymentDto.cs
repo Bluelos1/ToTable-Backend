950d3e9219e55261636e9d76aaf5fdbfc2c5dd60
Xerksez <s24895@pjwstk.edu.pl>
2024-01-21 20:18:37 +0100

    usuniete payment
    nie potrzebne

--- Contract/PaymentDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ToTable.Contract;
-
-public class PaymentDto
-{
-    public int PayId { get; set; }
-    public int PayCost { get; set; }
-    public string PayMethod { get; set; }
-    public string PayStatus { get; set; }
-}
