f843b2688dae5d5e46ddd21f238eca40589da70e
Bluelos1 <s25946@pjwstk.edu.pl>
2024-01-25 19:47:59 +0100

    Fix Waiter

--- Models/Waiter.cs
+++ Models/Waiter.cs
@@ -12,7 +12,9 @@ public class Waiter
     public string WaiterLogin { get; set; }
     public string WaiterPassw { get; set; }
     public bool IsAvailable { get; set; }
+    public bool IsAdmin { get; set; }
     public int RestaurantId { get; set; }
     public Restaurant? Restaurant { get; set; }
+    
     public ICollection<Order> Orders { get; set; }
 }
