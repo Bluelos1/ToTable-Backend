f45fd62946ab317fa2ba7e609f296242a138dc38
Bluelos1 <s25946@pjwstk.edu.pl>
2024-01-28 21:13:46 +0100

    Fix

--- Validator/WaiterValidator.cs
+++ Validator/WaiterValidator.cs
@@ -8,7 +8,7 @@ public class WaiterValidator : AbstractValidator<WaiterDto>
     public WaiterValidator()
     {
         RuleFor(x => x.RestaurantId).NotNull().GreaterThan(0).Must(x => x is int).WithMessage("Must be int");
-        RuleFor(x => x.IsAdmin).NotEmpty();
+        RuleFor(x => x.IsAdmin).NotNull();
         RuleFor(x => x.IsAvailable).NotNull();
         RuleFor(x => x.WaiterName).NotEmpty().Must(x => x is string).WithMessage("Must be string");
         RuleFor(x => x.WaiterLogin).NotEmpty().Must(x => x is string).WithMessage("Must be string");
