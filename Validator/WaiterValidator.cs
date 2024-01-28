5292a5eb8d0b3aae3efa018b0a15ffd50a13f423
Bluelos1 <s25946@pjwstk.edu.pl>
2024-01-28 21:22:13 +0100

    Fix validator

--- Validator/WaiterValidator.cs
+++ Validator/WaiterValidator.cs
@@ -10,10 +10,10 @@ public class WaiterValidator : AbstractValidator<WaiterDto>
         RuleFor(x => x.RestaurantId).NotNull().GreaterThan(0).Must(x => x is int).WithMessage("RestaurantId Must be int");
         RuleFor(x => x.IsAdmin).NotNull().WithMessage("IsAdmin Must be boolean");;
         RuleFor(x => x.IsAvailable).NotNull().WithMessage("IsAvailable Must be boolean");;
-        RuleFor(x => x.WaiterName).NotEmpty().Must(x => x is string).WithMessage("Must be string");
-        RuleFor(x => x.WaiterLogin).NotEmpty().Must(x => x is string).WithMessage("Must be string");
-        RuleFor(x => x.WaiterPassw).NotEmpty().Must(x => x is string).WithMessage("Must be string");
-        RuleFor(x => x.WaiterSurname).NotEmpty().Must(x => x is string).WithMessage("Must be string");
+        RuleFor(x => x.WaiterName).NotEmpty().Must(x => x is string).WithMessage("WaiterName Must be string");
+        RuleFor(x => x.WaiterLogin).NotEmpty().Must(x => x is string).WithMessage("WaiterLogin Must be string");
+        RuleFor(x => x.WaiterPassw).NotEmpty().Must(x => x is string).WithMessage("WaiterPassw Must be string");
+        RuleFor(x => x.WaiterSurname).NotEmpty().Must(x => x is string).WithMessage("WaiterSurname Must be string");
 
     }
 }
