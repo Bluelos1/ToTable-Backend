7ee062d8370dad0fa6d5c84ee8483c1036e38a7a
Bluelos1 <s25946@pjwstk.edu.pl>
2024-01-28 20:35:24 +0100

    Fix Fluent Validation

--- Validator/OrderValidator.cs
+++ Validator/OrderValidator.cs
@@ -10,8 +10,13 @@ public class OrderValidator : AbstractValidator<OrderDto>
     {
         RuleFor(x => x.OrderTime).NotNull();
         RuleFor(x => x.OrderStatus).IsInEnum().WithMessage("Put 0-New or 1-InProgress or 2-Done");
-        RuleFor(x => x.WaiterId).GreaterThan(0).Must(x => x is int).WithMessage("Must be int");
+        RuleFor(x => x.WaiterId).Must(x => x is int or null).WithMessage("Must be int or null");
         RuleFor(x => x.TableId).NotNull().GreaterThan(0).Must(x => x is int).WithMessage("Must be int");
         RuleFor(x => x.RestaurantId).NotNull().GreaterThan(0).Must(x => x is int).WithMessage("Must be int");
+        RuleFor(x => x.OrderComment).Must(x => x is string or null).WithMessage("Must be string or null");
+        RuleFor(x => x.PaymentMethod).Must(x => x is string or null).WithMessage("Must be string or null");
+        
+
+        
     }
 }
--- Validator/ProductValidator.cs
+++ Validator/ProductValidator.cs
@@ -13,6 +13,7 @@ public class ProductValidator : AbstractValidator<ProductDto>
         RuleFor(x => x.ImageUrl).NotEmpty().Must(x => x is string).WithMessage("Must be string");
         RuleFor(x => x.ProductCategory).IsInEnum().WithMessage("Chose 0-MainCourse, 1-Soup, 2-Soup, 3-Soup, 4-Beverages, 5-Extras");
         RuleFor(x => x.RestaurantId).NotNull().GreaterThan(0).Must(x => x is int).WithMessage("Must be int");
+        RuleFor(x => x.ProductStatus).NotEmpty().Must(x => x is string).WithMessage("Must be string");
 
     }
 }
